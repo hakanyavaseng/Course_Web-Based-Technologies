84f6c99f8bec76e84f6aecd402aee5b2f4e45211
Hakan Yavaş <hakanyavaseng@gmail.com>
2024-03-16 16:02:37 +0300

    homework 1, credit cart validator web app developed

---
+++ Homework-1/Credit-Card-Validator/Validator.aspx
@@ -0,0 +1,85 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="Validator.aspx.cs" Inherits="Credit_Card_Validator.Validator" %>
+
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.1//EN" "http://www.w3.org/TR/xhtml11/DTD/xhtml11.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head>
+    <title>Credit Card Verification</title>
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css" integrity="sha384-rbsA2VBKQhggwzxH7pPCaAqO46MgnOM80zW1RWuH61DGLwZJEdK2Kadq2F9CUG65" crossorigin="anonymous">
+    <style>
+        .error {
+            color: red;
+            font-weight: bold;
+            font-size: 0.8rem;
+            margin-top: 0.25rem;
+        }
+    </style>
+</head>
+<body>
+ <div class="d-flex align-items-lg-start m-lg-5"> <!-- Centering container -->
+    <form id="creditCardForm" runat="server" class="w-50 h-50">
+        <h1 class="mb-5">Credit Card Verification</h1>
+        <div class="row">
+            <div class="col-md-6 mb-3">
+                <label for="cardNumber" class="form-label">Card Number:</label>
+                <asp:TextBox ID="cardNumber" runat="server" CssClass="form-control"></asp:TextBox>
+                
+            </div>
+            <div class="col-md-6 mb-3">
+                <label for="cardholderName" class="form-label">Cardholder Name:</label>
+                <asp:TextBox ID="cardholderName" runat="server" CssClass="form-control"></asp:TextBox>
+            </div>
+            <div class="col-md-4 mb-3">
+                <label for="cardholderName" class="form-label">CVV:</label>
+                <asp:TextBox ID="cardCVV" runat="server" CssClass="form-control"></asp:TextBox>
+                
+            </div>
+            <div class="col-md-8 mb-3">
+                <label for="expirationMonth" class="form-label">Expiration Date:</label>
+                <div class="row">
+                    <div class="col-md-6">
+                        <asp:DropDownList ID="expirationMonth" runat="server" CssClass="form-select">
+                            <asp:ListItem Text="Month" Value="" Selected="True"></asp:ListItem>
+                            <asp:ListItem Text="January" Value="01"></asp:ListItem>
+                            <asp:ListItem Text="February" Value="02"></asp:ListItem>
+                            <asp:ListItem Text="March" Value="03"></asp:ListItem>
+                            <asp:ListItem Text="April" Value="04"></asp:ListItem>
+                            <asp:ListItem Text="May" Value="05"></asp:ListItem>
+                            <asp:ListItem Text="June" Value="06"></asp:ListItem>
+                            <asp:ListItem Text="July" Value="07"></asp:ListItem>
+                            <asp:ListItem Text="August" Value="08"></asp:ListItem>
+                            <asp:ListItem Text="September" Value="09"></asp:ListItem>
+                            <asp:ListItem Text="October" Value="10"></asp:ListItem>
+                            <asp:ListItem Text="November" Value="11"></asp:ListItem>
+                            <asp:ListItem Text="December" Value="12"></asp:ListItem>
+                        </asp:DropDownList>
+                       
+                    </div>
+                    <div class="col-md-6">
+                        <asp:DropDownList ID="expirationYear" runat="server" CssClass="form-select">
+                            <asp:ListItem Text="Year" Value="" Selected="True"></asp:ListItem>
+                            <asp:ListItem Text="2024" Value="2024"></asp:ListItem>
+                            <asp:ListItem Text="2025" Value="2025"></asp:ListItem>
+                            <asp:ListItem Text="2026" Value="2026"></asp:ListItem>
+                            <asp:ListItem Text="2027" Value="2027"></asp:ListItem>
+                            <asp:ListItem Text="2028" Value="2028"></asp:ListItem>
+                            <asp:ListItem Text="2029" Value="2029"></asp:ListItem>
+                            <asp:ListItem Text="2030" Value="2030"></asp:ListItem>
+                        </asp:DropDownList>
+                       
+                    </div>
+                </div>
+            </div>
+            <div class="col-md-6 mb-3">
+                <label class="form-label">Card Type:</label>
+                <asp:Image ID="cardTypeImage" runat="server" CssClass="card-type-image" Width="50" Height="50" />
+            </div>
+            <div class="col-12">
+                <asp:Button ID="submitButton" runat="server" Text="Verify Card" CssClass="btn btn-primary" OnClick="SubmitButton_Click" />
+                <asp:Label ID="errorMessageLabel" runat="server" CssClass="error" Visible="false"></asp:Label>
+            </div>
+        </div>
+    </form>
+</div>
+
+</body>
+</html>
---
+++ Homework-1/Credit-Card-Validator/Validator.aspx.cs
@@ -0,0 +1,156 @@
+﻿using System;
+using System.Linq;
+using System.Text.RegularExpressions;
+using static Credit_Card_Validator.CreditCardValidator;
+
+namespace Credit_Card_Validator
+{
+    public partial class Validator : System.Web.UI.Page
+    {
+        CreditCardValidator validator = new CreditCardValidator();
+        protected void Page_Load(object sender, EventArgs e)
+        {
+        }
+
+        protected void SubmitButton_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                cardTypeImage.ImageUrl = "";
+                // Validating Credit Card Number 
+                if (cardNumber.Text.Length == 0)
+                    throw new Exception(validator.GetErrorMessage(2));
+                if (cardNumber.Text.Length != 16)
+                    throw new Exception(validator.GetErrorMessage(5));
+
+                // Validating Cardholder Name
+                if (!Regex.IsMatch(cardholderName.Text, @"^[a-zA-Z\s]+$"))
+                    throw new Exception(validator.GetErrorMessage(5));
+
+                // Validating CVV length
+                if (cardCVV.Text.Length != 3)
+                    throw new Exception(validator.GetErrorMessage(6));
+
+                // Validating Expiration Date
+                if (expirationMonth.SelectedIndex == 0 || expirationYear.SelectedIndex == 0)
+                    throw new Exception(validator.GetErrorMessage(8));
+
+                // Validating Credit Card Number
+                var result = validator.validateCreditCardNumber(creditCardNumber: cardNumber.Text);
+                if (result==true) // Validated
+                {
+                    string creditCardType = validator.GetCardType(cardNumber.Text);
+                    if (creditCardType == CardType.Mastercard.ToString())
+                    {
+                        cardTypeImage.ImageUrl = "https://w7.pngwing.com/pngs/962/794/png-transparent-mastercard-credit-card-mastercard-logo-mastercard-logo-love-text-heart.png";
+                    }
+                    else if (creditCardType == CardType.Visa.ToString())
+                    {
+                        cardTypeImage.ImageUrl = "https://logowik.com/content/uploads/images/t_visa-payment-card1873.jpg";
+                    }
+                    else
+                    {
+                        cardTypeImage.ImageUrl = "https://upload.wikimedia.org/wikipedia/commons/e/e2/Unknown_toxicity_icon.png";
+                    }
+
+                    errorMessageLabel.Visible = false;
+                }
+                else
+                {
+                    throw new Exception(validator.GetErrorMessage(1));
+                }
+            }
+            catch (Exception ex)
+            {
+                errorMessageLabel.Text = ex.Message;
+                errorMessageLabel.Visible = true;
+            }
+        }
+
+    }
+    class CreditCardValidator
+    {
+        public bool validateCreditCardNumber(string creditCardNumber)
+        {
+            string cleanedNumber = string.Concat(creditCardNumber.Where(char.IsDigit));
+
+            if (string.IsNullOrEmpty(cleanedNumber))
+            {
+                return false;
+            }
+
+            int sum = 0;
+            bool isSecondDigit = false;
+
+            for (int i = cleanedNumber.Length - 1; i >= 0; i--)
+            {
+                int digit = cleanedNumber[i] - '0';
+
+                // Double every second digit
+                if (isSecondDigit)
+                {
+                    digit *= 2;
+                    if (digit > 9)
+                    {
+                        digit = digit % 10 + 1;
+                    }
+                }
+                sum += digit;
+                isSecondDigit = !isSecondDigit;
+            }
+
+            bool isValid = sum % 10 == 0;
+
+            return isValid;
+        }
+
+        public string GetCardType(string cardNumber)
+        {
+            string[] visaPrefixes = { "4" };
+            string[] mastercardPrefixes = { "51", "52", "53", "54", "55" };
+
+            if (visaPrefixes.Any(prefix => cardNumber.StartsWith(prefix)))
+            {
+                return CardType.Visa.ToString();
+            }
+
+            if (mastercardPrefixes.Any(prefix => cardNumber.StartsWith(prefix)))
+            {
+                return CardType.Mastercard.ToString();
+            }
+            return CardType.Unknown.ToString();
+        }
+
+        public enum CardType
+        {
+            Visa,
+            Mastercard,
+            Unknown
+        }
+
+        public string GetErrorMessage(int code)
+        {
+            switch (code)
+            {
+                case 1:
+                    return "Unknown card type";
+                case 2:
+                    return "No card number provided";
+                case 3:
+                    return "Credit card number is in invalid format";
+                case 4:
+                    return "Credit card number is invalid";
+                case 5:
+                    return "Invalid cardholder name.";
+                case 6:
+                    return "Invalid CVV length.";
+                case 7:
+                    return "Credit card number has an inappropriate number of digits";
+                case 8:
+                    return "Expiration date is invalid.";
+                default:
+                    return "Unknown error";
+            }
+        }
+    }
+}
